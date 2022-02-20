b126bbfb1a77f562430bcf9f776b3d6f74e58301
Nam Tran H.[2] VN.Hanoi <nam.tran2@monstar-lab.com>
2022-02-20 22:58:01 +0700

    make it better7

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect(
+    mongoose.connect(
       "mongodb+srv://nam18020938:tS6B1fs2tVuGsKtD@cluster0.tdb8j.mongodb.net/18020938_do_an?authSource=admin&replicaSet=atlas-11n93s-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
       {
         useNewUrlParser: true,
