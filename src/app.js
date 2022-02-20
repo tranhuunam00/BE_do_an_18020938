69fab06c385410dcbb3051742b89df9682a9f5df
Nam Tran H.[2] VN.Hanoi <nam.tran2@monstar-lab.com>
2022-02-20 19:51:15 +0700

    make it better4

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-const logger = require("./utils/logger");
+// const logger = require("./utils/logger");
 require("dotenv").config();
 
 //datebase --mongo
