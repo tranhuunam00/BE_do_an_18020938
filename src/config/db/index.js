34def6f3f027d3b6036e4dfcd00cb748e091feed
Nam Tran H.[2] VN.Hanoi <nam.tran2@monstar-lab.com>
2022-02-20 23:07:26 +0700

    make it better5

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     mongoose.connect(
-      `mongodb+srv://nam18020938:tS6B1fs2tVuGsKtD@cluster0.tdb8j.mongodb.net/18020938_do_an`,
+      "mongodb+srv://nam18020938:tS6B1fs2tVuGsKtD@cluster0.tdb8j.mongodb.net/18020938_do_an?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
