47e0e62a7cbb0900bf869555b97e096bf44187d1
Nam Tran H.[2] VN.Hanoi <nam.tran2@monstar-lab.com>
2022-02-20 19:25:11 +0700

    make it better

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 async function connect() {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://udic-admin:gUi6Trs4Vqq3YNG@udic-dev.kzbr4.mongodb.net/udic-dev?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: false,
-        // useCreateIndex: true,
-      }
-    );
-    console.log("connect mongo db done!");
-  } catch (e) {
-    console.log(e.message);
-  }
+  // try {
+  //   await mongoose.connect(
+  //     "mongodb+srv://udic-admin:gUi6Trs4Vqq3YNG@udic-dev.kzbr4.mongodb.net/udic-dev?retryWrites=true&w=majority",
+  //     {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //       // useFindAndModify: false,
+  //       // useCreateIndex: true,
+  //     }
+  //   );
+  //   console.log("connect mongo db done!");
+  // } catch (e) {
+  //   console.log(e.message);
+  // }
 }
 module.exports = {
   connect,
