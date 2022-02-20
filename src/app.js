87ab564fb1f9a0ae9da40a89d2747d6ddbcf77b7
Nam Tran H.[2] VN.Hanoi <nam.tran2@monstar-lab.com>
2022-02-20 19:41:11 +0700

    make it better3

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use("/public", express.static(path.join(__dirname, "./public")));
 app.use("/docs", express.static(path.join(__dirname, "docs")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./home.html"));
+  res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
